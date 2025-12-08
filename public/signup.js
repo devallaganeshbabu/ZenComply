bd7d8350e5a206c1ff226fdedc4c7babb424f0c3
Devalla Ganeshbabu <141542835+devallaganeshbabu@users.noreply.github.com>
2025-12-08 02:09:16 +0000

    Update API_BASE_URL in signup.js

--- public/signup.js
+++ public/signup.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://zencomply.onrender.com/api";
+const API_BASE_URL = "https://zencomply.onrender.com";
 
 document.getElementById("signupForm").addEventListener("submit", async (e) => {
     e.preventDefault();
